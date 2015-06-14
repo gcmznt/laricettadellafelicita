6090efb88fd99dbfc2a772fe63ad541d54c09e45
Giacomo Zinetti <giacomo.zinetti@giko.it>
2015-06-14 11:26:02 +0200

    maps

--- src/assets/js/maps.js
+++ src/assets/js/maps.js
@@ -1,40 +1,36 @@
 
-// let createMap = (id, coord, title) => {
-//   let m = new google.maps.Map(document.getElementById(id), {
-//     zoom: 13,
-//     scrollwheel: false,
-//     center: coord
-//   });
-//   new google.maps.Marker({
-//     position: coord,
-//     map: m,
-//     title: title
-//   });
-//   return m;
-// }
-
-
-// google.maps.event.addDomListener(window, 'load', () => {
-//   window.mapVignola = createMap(
-//     'Map--party',
-//     new google.maps.LatLng(45.681387, 9.372540),
-//     'Cascina Vignola'
-//   );
-//   window.mapChurch = createMap(
-//     'Map--church',
-//     new google.maps.LatLng(45.631006, 9.168113),
-//     'S. Pio X'
-//   );
-// });
-
-
-(function($) {
-
-  $(document).on('click', 'a[href^=#Map--]', function(event) {
-    event.preventDefault();
-    // TODO MAP
-    // $('[id^=Map--]:visible').slideToggle()
-    // $($(this).attr('href')).slideToggle();
+let createMap = (id, markers) => {
+  let bounds = new google.maps.LatLngBounds();
+  let m = new google.maps.Map(document.getElementById(id), {
+    zoom: 13,
+    scrollwheel: false
   });
+  for (let i = markers.length - 1; i >= 0; i--) {
+    let marker = new google.maps.Marker({
+      map: m,
+      position: markers[i].pos,
+      title: markers[i].title
+    });
+    bounds.extend(marker.position);
+  }
+  m.fitBounds(bounds);
+  return m;
+}
 
-})(jQuery);
+
+google.maps.event.addDomListener(window, 'load', () => {
+  window.map = createMap(
+    'Map',
+    [
+      {
+        pos: new google.maps.LatLng(45.681387, 9.372540),
+        title: 'Cascina Vignola'
+      },
+      {
+        pos: new google.maps.LatLng(45.631006, 9.168113),
+        title: 'S. Pio X'
+      }
+    ]
+  );
+});
+
