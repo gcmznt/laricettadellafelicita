eeab30ce1c4d586a95099671e98e939b6a3d8e0b
Giacomo Zinetti <giacomo.zinetti@giko.it>
2015-07-21 22:20:17 +0200

    test cloud

---
+++ src/assets/js/clouds.js
@@ -0,0 +1,10 @@
+
+// (function($){
+//   var generateCloud = function() {
+//     $('<div/>', {
+//         class: 'Trip__cloud'
+//     }).prependTo('.Trip');
+//   };
+
+//   generateCloud();
+// })(jQuery);
