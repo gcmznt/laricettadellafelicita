5a62407f31d79a3ff4f2e6b4d74af1e989cddb8d
Giacomo Zinetti <giacomo.zinetti@giko.it>
2016-03-07 22:24:13 +0100

    post viaggio

--- gulptasks/rsync.js
+++ gulptasks/rsync.js
@@ -5,9 +5,9 @@ var rsync = require("gulp-rsync");
 module.exports = function () {
   return gulp.src("dist/")
     .pipe(rsync({
-      destination: '/var/vhost/laricettadellafelicita/',
+      destination: '/var/vhost/laricettadellafelicita/docroot/',
       root: 'dist/',
-      hostname: '95.85.31.194',
+      hostname: '46.101.225.6',
       username: 'root',
       incremental: true,
       progress: true,
